7d9eafd699da220a242a2fb306079ee06ed44ecc
Menno Drescher <menno.drescher@gmail.com>
2025-05-21 16:14:01 +0200

    Update AssemblyInfo files with new informational version and add solution file for project structure

--- api/api.AppHost/obj/Debug/net8.0/api.AppHost.AssemblyInfo.cs
+++ api/api.AppHost/obj/Debug/net8.0/api.AppHost.AssemblyInfo.cs
@@ -23,7 +23,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api.AppHost")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a379ebde5b3cade346825e2c6f61a4a2227e772c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bcc83635687c100460bd3c94594435031beef643")]
 [assembly: System.Reflection.AssemblyProductAttribute("api.AppHost")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api.AppHost")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- api/api.ServiceDefaults/obj/Debug/net8.0/api.ServiceDefaults.AssemblyInfo.cs
+++ api/api.ServiceDefaults/obj/Debug/net8.0/api.ServiceDefaults.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api.ServiceDefaults")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a379ebde5b3cade346825e2c6f61a4a2227e772c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bcc83635687c100460bd3c94594435031beef643")]
 [assembly: System.Reflection.AssemblyProductAttribute("api.ServiceDefaults")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api.ServiceDefaults")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
