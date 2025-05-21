58c1ca466b473792e369a2c0e26d5cdde6346e6b
Menno Drescher <menno.drescher@gmail.com>
2025-05-21 22:09:03 +0200

    Update assembly informational version and add installation guide

---
+++ INSTALLATION.md
@@ -0,0 +1,32 @@
+## Installation Guide
+
+### Prerequisites
+Ensure you have the following installed:
+- Node.js
+- npm
+- Visual Studio Code
+
+### Steps to Install
+1. Clone the repository:
+   ```bash
+   git clone https://github.com/mdresch/my-portfolio-menno.git
+   ```
+
+2. Navigate to the project directory:
+   ```bash
+   cd my-portfolio-menno
+   ```
+
+3. Install dependencies:
+   ```bash
+   npm install
+   ```
+
+4. Run the project:
+   ```bash
+   npm start
+   ```
+
+### Additional Setup
+5. - Configure environment variables if needed.
+
--- api/api.AppHost/obj/Debug/net8.0/api.AppHost.AssemblyInfo.cs
+++ api/api.AppHost/obj/Debug/net8.0/api.AppHost.AssemblyInfo.cs
@@ -23,7 +23,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api.AppHost")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51c74c34a57330db1b3565aa208594fd46e472a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21364ba397fc55aa201f0e4e823d45e072601414")]
 [assembly: System.Reflection.AssemblyProductAttribute("api.AppHost")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api.AppHost")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
