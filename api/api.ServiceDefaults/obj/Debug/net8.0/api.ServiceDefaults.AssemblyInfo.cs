c8318a9eca05acb3574153fdb0e5a861b6e797ff
Menno Drescher <menno.drescher@gmail.com>
2025-05-31 14:39:09 +0200

    Add blog post on self-charging cars and recent industry developments - May 31, 2025

--- api/api.ServiceDefaults/obj/Debug/net8.0/api.ServiceDefaults.AssemblyInfo.cs
+++ api/api.ServiceDefaults/obj/Debug/net8.0/api.ServiceDefaults.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api.ServiceDefaults")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e8b23207cc8b286c50b44dde72f957abafd58de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8cbe625e9a6174afe6ed58643671369b47489a3b")]
 [assembly: System.Reflection.AssemblyProductAttribute("api.ServiceDefaults")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api.ServiceDefaults")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
